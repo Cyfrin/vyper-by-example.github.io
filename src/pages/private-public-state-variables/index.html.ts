dd676ee3a125dccb24021747826ce0848c6cf0e8
t4sk <tsk.nakamura@gmail.com>
2022-07-24 17:50:52 +0900

    fix

--- src/pages/private-public-state-variables/index.html.ts
+++ src/pages/private-public-state-variables/index.html.ts
@@ -5,7 +5,20 @@ export const description = "Private and public state variables in Vyper"
 
 const html = `<p>Private state variables cannot be accessed from outside the contract.</p>
 <p>Public state variables can be read by anyone, including users and other contracts.</p>
-<pre><code class="language-vyper">
+<pre><code class="language-vyper"><span class="hljs-comment"># @version ^0.3.3</span>
+
+<span class="hljs-comment"># publis state variable</span>
+owner: public(address)
+<span class="hljs-comment"># private state variable</span>
+foo: uint256
+bar: public(<span class="hljs-built_in">bool</span>)
+
+
+<span class="hljs-meta">@external</span>
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">__init__</span>():
+    self.owner = msg.sender
+    self.foo = <span class="hljs-number">123</span>
+    self.bar = <span class="hljs-literal">True</span>
 </code></pre>
 `
 
