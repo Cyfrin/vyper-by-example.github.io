fc8e226396a9a9261857af14716a5b1f030f5b7e
t4sk <tsk.nakamura@gmail.com>
2022-07-24 17:03:25 +0900

    content

---
+++ src/pages/payable/index.html.ts
@@ -0,0 +1,38 @@
+// metadata
+export const version = "0.3.3"
+export const title = "Payable"
+export const description = "Payable in Vyper"
+
+const html = `<pre><code class="language-vyper"><span class="hljs-comment"># @version ^0.3.3</span>
+
+
+event Deposit:
+    sender: indexed(address)
+    amount: uint256
+
+
+<span class="hljs-meta">@external</span>
+<span class="hljs-meta">@payable</span>
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">deposit</span>():
+    log Deposit(msg.sender, msg.value)
+
+
+<span class="hljs-meta">@external</span>
+<span class="hljs-meta">@view</span>
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">getBalance</span>() -&gt; uint256:
+    <span class="hljs-comment"># Get balance of Ether stored in this contract</span>
+    <span class="hljs-keyword">return</span> self.balance
+
+
+owner: public(address)
+
+
+<span class="hljs-meta">@external</span>
+<span class="hljs-meta">@payable</span>
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">pay</span>():
+    <span class="hljs-keyword">assert</span> msg.value &gt; <span class="hljs-number">0</span>, <span class="hljs-string">"msg.value = 0"</span>
+    self.owner = msg.sender
+</code></pre>
+`
+
+export default html
---
+++ src/pages/payable/index.md
@@ -0,0 +1,9 @@
+---
+title: Payable
+version: 0.3.3
+description: Payable in Vyper
+---
+
+```vyper
+{{{Payable}}}
+```
