58aba814dea580cd4f10cc0ac61928819705d560
t4sk <tsk.nakamura@gmail.com>
2023-09-22 19:01:52 +0900

    hamburger svg

---
+++ src/components/svg/Hamburger.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+interface Props {
+  size: number
+  className?: string
+}
+
+const Hamburger: React.FC<Props> = ({ size, className = "" }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 20 20"
+      strokeWidth={2}
+      stroke="currentColor"
+      className={className}
+      width={size}
+      height={size}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  )
+}
+
+export default Hamburger
