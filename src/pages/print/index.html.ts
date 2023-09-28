4b0226ee73cff25000174c47ed300e56199448c6
t4sk <tsk.nakamura@gmail.com>
2023-09-28 20:35:10 +0900

    lint

--- src/pages/print/index.html.ts
+++ src/pages/print/index.html.ts
@@ -3,10 +3,7 @@ export const version = "0.3.9"
 export const title = "Debug with Print"
 export const description = "Debug with Print"
 
-export const keywords = [
-    "debug",
-    "print",
-]
+export const keywords = ["debug", "print"]
 
 const html = `<p>Use the built-in function <code>print</code> to debug smart contracts.</p>
 <pre><code class="language-vyper"><span class="hljs-comment"># @version ^0.3.9</span>
