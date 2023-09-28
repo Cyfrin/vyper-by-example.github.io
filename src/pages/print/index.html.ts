95450801be30b1081d5f3705456f2780e7d3962e
t4sk <tsk.nakamura@gmail.com>
2023-09-28 10:43:26 +0900

    print

--- src/pages/print/index.html.ts
+++ src/pages/print/index.html.ts
@@ -3,10 +3,21 @@ export const version = "0.3.9"
 export const title = "Debug with Print"
 export const description = "Debug with Print"
 
-export const keywords = ["debug", "print"]
+export const keywords = [
+    "debug",
+    "print",
+]
 
 const html = `<p>Use the built-in function <code>print</code> to debug smart contracts.</p>
-<pre><code class="language-vyper">
+<pre><code class="language-vyper"><span class="hljs-comment"># @version ^0.3.9</span>
+
+<span class="hljs-meta">@external</span>
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">test_print</span>():
+    x: uint256 = <span class="hljs-number">123</span>
+    <span class="hljs-comment"># Set hardhat_compat = True when testing with Hardhat</span>
+    <span class="hljs-built_in">print</span>(<span class="hljs-string">"print something here"</span>, x, hardhat_compat=<span class="hljs-literal">True</span>)
+
+
 </code></pre>`
 
 export default html
