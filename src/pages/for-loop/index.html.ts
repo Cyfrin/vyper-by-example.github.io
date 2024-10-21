7245ac56e4d477593957d042a6f8121ba1680b6c
t4sk <tsk.nakamura@gmail.com>
2024-10-21 19:41:09 +0900

    for loop

--- src/pages/for-loop/index.html.ts
+++ src/pages/for-loop/index.html.ts
@@ -10,34 +10,49 @@ const html = `<p>There are 2 ways to loop through an array, using <code>range</c
 
 <span class="hljs-meta">@external</span>
 <span class="hljs-meta">@pure</span>
-<span class="hljs-keyword">def</span> <span class="hljs-title function_">for_loop</span>():
-    <span class="hljs-comment"># for loop through range</span>
-    <span class="hljs-comment"># sum from 0 to 9</span>
-    s: uint256 = <span class="hljs-number">0</span>
-    <span class="hljs-keyword">for</span> i: uint256 <span class="hljs-keyword">in</span> <span class="hljs-built_in">range</span>(<span class="hljs-number">10</span>):
-        s += i
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">for_loop</span>() -&gt; DynArray[uint256, <span class="hljs-number">10</span>]:
+    arr: DynArray[uint256, <span class="hljs-number">10</span>] = []
 
-    <span class="hljs-comment"># for loop through array elements</span>
-    <span class="hljs-comment"># find minimum of nums</span>
-    nums: uint256[<span class="hljs-number">5</span>] = [<span class="hljs-number">4</span>, <span class="hljs-number">5</span>, <span class="hljs-number">1</span>, <span class="hljs-number">9</span>, <span class="hljs-number">0</span>]
-    x: uint256 = max_value(uint256)
-    <span class="hljs-keyword">for</span> num: uint256 <span class="hljs-keyword">in</span> nums:
-        <span class="hljs-keyword">if</span> x &lt; num:
-            x = num
+    <span class="hljs-keyword">for</span> i: uint256 <span class="hljs-keyword">in</span> <span class="hljs-built_in">range</span>(<span class="hljs-number">5</span>):
+        arr.append(i)
 
-    <span class="hljs-comment"># continue - skip current iteration</span>
-    <span class="hljs-comment"># break - exit for loop</span>
-    c: uint256 = <span class="hljs-number">0</span>
-    <span class="hljs-keyword">for</span> i: uint256 <span class="hljs-keyword">in</span> [<span class="hljs-number">1</span>, <span class="hljs-number">2</span>, <span class="hljs-number">3</span>, <span class="hljs-number">4</span>, <span class="hljs-number">5</span>]:
-        <span class="hljs-comment"># skip 2</span>
+    <span class="hljs-keyword">return</span> arr
+
+<span class="hljs-meta">@external</span>
+<span class="hljs-meta">@pure</span>
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">for_loop_start_end</span>() -&gt; DynArray[uint256, <span class="hljs-number">10</span>]:
+    arr: DynArray[uint256, <span class="hljs-number">10</span>] = []
+
+    <span class="hljs-keyword">for</span> i: uint256 <span class="hljs-keyword">in</span> <span class="hljs-built_in">range</span>(<span class="hljs-number">5</span>, <span class="hljs-number">10</span>):
+        arr.append(i)
+
+    <span class="hljs-keyword">return</span> arr
+
+<span class="hljs-meta">@external</span>
+<span class="hljs-meta">@pure</span>
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">for_loop_list</span>() -&gt; DynArray[uint256, <span class="hljs-number">10</span>]:
+    arr: DynArray[uint256, <span class="hljs-number">10</span>] = []
+
+    nums: uint256[<span class="hljs-number">4</span>] = [<span class="hljs-number">11</span>, <span class="hljs-number">22</span>, <span class="hljs-number">33</span>, <span class="hljs-number">44</span>]
+
+    <span class="hljs-keyword">for</span> i: uint256 <span class="hljs-keyword">in</span> nums:
+        arr.append(i)
+
+    <span class="hljs-keyword">return</span> arr
+
+<span class="hljs-meta">@external</span>
+<span class="hljs-meta">@pure</span>
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">for_loop_skip</span>() -&gt; DynArray[uint256, <span class="hljs-number">10</span>]:
+    arr: DynArray[uint256, <span class="hljs-number">10</span>] = []
+
+    <span class="hljs-keyword">for</span> i: uint256 <span class="hljs-keyword">in</span> <span class="hljs-built_in">range</span>(<span class="hljs-number">5</span>):
         <span class="hljs-keyword">if</span> i == <span class="hljs-number">2</span>:
             <span class="hljs-keyword">continue</span>
-        <span class="hljs-comment"># end loop at i = 4</span>
         <span class="hljs-keyword">if</span> i == <span class="hljs-number">4</span>:
             <span class="hljs-keyword">break</span>
-        <span class="hljs-comment"># otherwise increment c by 1</span>
-        c += <span class="hljs-number">1</span>
-    <span class="hljs-comment"># c = 2 (incremented at i = 1 and 3)</span>
+        arr.append(i)
+
+    <span class="hljs-keyword">return</span> arr
 
 <span class="hljs-meta">@external</span>
 <span class="hljs-meta">@pure</span>
