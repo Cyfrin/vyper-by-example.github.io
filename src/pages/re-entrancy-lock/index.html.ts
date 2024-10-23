8da76041b91cc1c926b2fb18c3b4559aa81300a1
t4sk <tsk.nakamura@gmail.com>
2024-10-24 08:25:59 +0900

    update

--- src/pages/re-entrancy-lock/index.html.ts
+++ src/pages/re-entrancy-lock/index.html.ts
@@ -8,6 +8,7 @@ export const keywords = ["re-entrancy", "lock", "nonreentrant"]
 const html = `<p>Vyper has a handy way to secure your contract from re-entrancy.</p>
 <p>A re-entrancy lock can be created on a function with <code>@nonreentrant</code>.</p>
 <p>Functions can be locked together by using the same name for the locks.</p>
+<p>Contract with re-entrancy lock</p>
 <pre><code class="language-vyper"><span class="hljs-comment"># pragma version ^0.4.0</span>
 
 <span class="hljs-meta">@external</span>
@@ -15,13 +16,15 @@ const html = `<p>Vyper has a handy way to secure your contract from re-entrancy.
 <span class="hljs-keyword">def</span> <span class="hljs-title function_">func</span>():
     raw_call(msg.sender, <span class="hljs-string">b""</span>, value=<span class="hljs-number">0</span>)
 
-</code></pre><pre><code class="language-vyper"><span class="hljs-comment"># pragma version ^0.4.0</span>
+</code></pre><p>Contract without re-entrancy lock</p>
+<pre><code class="language-vyper"><span class="hljs-comment"># pragma version ^0.4.0</span>
 
 <span class="hljs-meta">@external</span>
 <span class="hljs-keyword">def</span> <span class="hljs-title function_">func</span>():
     raw_call(msg.sender, <span class="hljs-string">b""</span>, value=<span class="hljs-number">0</span>)
 
-</code></pre><pre><code class="language-vyper"><span class="hljs-comment"># pragma version ^0.4.0</span>
+</code></pre><p>Contract to test re-entrancy</p>
+<pre><code class="language-vyper"><span class="hljs-comment"># pragma version ^0.4.0</span>
 
 interface ILock:
     <span class="hljs-keyword">def</span> <span class="hljs-title function_">func</span>(): nonpayable
