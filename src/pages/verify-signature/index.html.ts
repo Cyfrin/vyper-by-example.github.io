b8ca9a790f170f8f0dccb30c90ad8e29802dd782
t4sk <tsk.nakamura@gmail.com>
2024-10-26 08:55:31 +0900

    sig

--- src/pages/verify-signature/index.html.ts
+++ src/pages/verify-signature/index.html.ts
@@ -13,28 +13,25 @@ const html = `<p>Signing and verifying signature takes 3 steps.</p>
 </ol>
 <pre><code class="language-vyper"><span class="hljs-comment"># pragma version ^0.4.0</span>
 
-<span class="hljs-comment"># hash = get_hash("Hello Vyper!")</span>
-<span class="hljs-comment"># 0x5436c86f18e3d25a10e557ae125450118dd0a481ca22112b1977d55a676e4c91</span>
-<span class="hljs-meta">@external</span>
-<span class="hljs-meta">@pure</span>
-<span class="hljs-keyword">def</span> <span class="hljs-title function_">get_hash</span>(<span class="hljs-params"><span class="hljs-built_in">str</span>: String[<span class="hljs-number">100</span>]</span>) -&gt; bytes32:
-    <span class="hljs-keyword">return</span> keccak256(<span class="hljs-built_in">str</span>)
+<span class="hljs-comment"># 1. Hash data to sign</span>
+<span class="hljs-comment"># 2. Sign data offchain</span>
+<span class="hljs-comment"># 3. Verify signature onchain</span>
 
-<span class="hljs-comment"># get_eth_signed_hash(hash)</span>
-<span class="hljs-comment"># 0x045b623a8e8fb7b4fcfbd1ae07e7326d55303d7de4085c69b346bd130c1936da</span>
 <span class="hljs-meta">@external</span>
 <span class="hljs-meta">@pure</span>
-<span class="hljs-keyword">def</span> <span class="hljs-title function_">get_eth_signed_hash</span>(<span class="hljs-params"><span class="hljs-built_in">hash</span>: bytes32</span>) -&gt; bytes32:
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">get_hash</span>(<span class="hljs-params">msg_str: String[<span class="hljs-number">100</span>]</span>) -&gt; bytes32:
+    <span class="hljs-keyword">return</span> keccak256(msg_str)
+
+<span class="hljs-meta">@external</span>
+<span class="hljs-meta">@pure</span>
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">get_eth_hash</span>(<span class="hljs-params">msg_hash: bytes32</span>) -&gt; bytes32:
     <span class="hljs-keyword">return</span> keccak256(
         concat(
             <span class="hljs-string">b&#x27;\\x19Ethereum Signed Message:\\n32&#x27;</span>,
-            <span class="hljs-built_in">hash</span>
+            msg_hash
         )
     )
 
-<span class="hljs-comment"># account = your account</span>
-<span class="hljs-comment"># hash = get_hash("Hello Vyper!")</span>
-<span class="hljs-comment"># signature = await ethereum.request({ method: "personal_sign", params: [account, hash]})</span>
 <span class="hljs-meta">@external</span>
 <span class="hljs-meta">@pure</span>
 <span class="hljs-keyword">def</span> <span class="hljs-title function_">recover_signer</span>(<span class="hljs-params">eth_signed_hash: bytes32, sig: Bytes[<span class="hljs-number">65</span>]</span>) -&gt; address:
@@ -43,22 +40,24 @@ const html = `<p>Signing and verifying signature takes 3 steps.</p>
     v: uint256 = convert(<span class="hljs-built_in">slice</span>(sig, <span class="hljs-number">64</span>, <span class="hljs-number">1</span>), uint256)
     <span class="hljs-keyword">return</span> ecrecover(eth_signed_hash, v, r, s)
 
+<span class="hljs-comment"># Elliptic Curve Digital Signature Algorithm (ECDSA)</span>
+<span class="hljs-comment"># accounts = await ethereum.request({ method: "eth_requestAccounts" })</span>
+<span class="hljs-comment"># account = accounts[0]</span>
+<span class="hljs-comment"># h = get_hash("vyper")</span>
+<span class="hljs-comment"># signature = await ethereum.request({ method: "personal_sign", params: [account, h]})</span>
 <span class="hljs-meta">@external</span>
 <span class="hljs-meta">@pure</span>
-<span class="hljs-keyword">def</span> <span class="hljs-title function_">verify</span>(<span class="hljs-params">sig: Bytes[<span class="hljs-number">65</span>], <span class="hljs-built_in">str</span>: String[<span class="hljs-number">100</span>], _signer: address</span>) -&gt; <span class="hljs-built_in">bool</span>:
-    <span class="hljs-built_in">hash</span>: bytes32 = keccak256(<span class="hljs-built_in">str</span>)
-    eth_signed_hash: bytes32 = keccak256(
-        concat(
-            <span class="hljs-string">b&#x27;\\x19Ethereum Signed Message:\\n32&#x27;</span>,
-            <span class="hljs-built_in">hash</span>
-        )
+<span class="hljs-keyword">def</span> <span class="hljs-title function_">verify</span>(<span class="hljs-params">sig: Bytes[<span class="hljs-number">65</span>], msg_str: String[<span class="hljs-number">100</span>], signer: address</span>) -&gt; <span class="hljs-built_in">bool</span>:
+    h: bytes32 = keccak256(msg_str)
+    eth_hash: bytes32 = keccak256(
+        concat(<span class="hljs-string">b"\\x19Ethereum Signed Message:\\n32"</span>, h)
     )
     r: uint256 = convert(<span class="hljs-built_in">slice</span>(sig, <span class="hljs-number">0</span>, <span class="hljs-number">32</span>), uint256)
     s: uint256 = convert(<span class="hljs-built_in">slice</span>(sig, <span class="hljs-number">32</span>, <span class="hljs-number">32</span>), uint256)
     v: uint256 = convert(<span class="hljs-built_in">slice</span>(sig, <span class="hljs-number">64</span>, <span class="hljs-number">1</span>), uint256)
-    signer: address = ecrecover(eth_signed_hash, v, r, s)
+    recovered_addr: address = ecrecover(eth_hash, v, r, s)
+    <span class="hljs-keyword">return</span> recovered_addr == signer
 
-    <span class="hljs-keyword">return</span> _signer == signer
 </code></pre>`
 
 export default html
--- vy/verify-signature/VerifySig.vy
+++ vy/verify-signature/VerifySig.vy
@@ -1,27 +1,24 @@
 # pragma version ^0.4.0
 
-# hash = get_hash("Hello Vyper!")
-# 0x5436c86f18e3d25a10e557ae125450118dd0a481ca22112b1977d55a676e4c91
-@external
-@pure
-def get_hash(str: String[100]) -> bytes32:
-    return keccak256(str)
+# 1. Hash data to sign
+# 2. Sign data offchain
+# 3. Verify signature onchain
 
-# get_eth_signed_hash(hash)
-# 0x045b623a8e8fb7b4fcfbd1ae07e7326d55303d7de4085c69b346bd130c1936da
 @external
 @pure
-def get_eth_signed_hash(hash: bytes32) -> bytes32:
+def get_hash(msg_str: String[100]) -> bytes32:
+    return keccak256(msg_str)
+
+@external
+@pure
+def get_eth_hash(msg_hash: bytes32) -> bytes32:
     return keccak256(
         concat(
             b'\x19Ethereum Signed Message:\n32',
-            hash
+            msg_hash
         )
     )
 
-# account = your account
-# hash = get_hash("Hello Vyper!")
-# signature = await ethereum.request({ method: "personal_sign", params: [account, hash]})
 @external
 @pure
 def recover_signer(eth_signed_hash: bytes32, sig: Bytes[65]) -> address:
@@ -30,19 +27,21 @@ def recover_signer(eth_signed_hash: bytes32, sig: Bytes[65]) -> address:
     v: uint256 = convert(slice(sig, 64, 1), uint256)
     return ecrecover(eth_signed_hash, v, r, s)
 
+# Elliptic Curve Digital Signature Algorithm (ECDSA)
+# accounts = await ethereum.request({ method: "eth_requestAccounts" })
+# account = accounts[0]
+# h = get_hash("vyper")
+# signature = await ethereum.request({ method: "personal_sign", params: [account, h]})
 @external
 @pure
-def verify(sig: Bytes[65], str: String[100], _signer: address) -> bool:
-    hash: bytes32 = keccak256(str)
-    eth_signed_hash: bytes32 = keccak256(
-        concat(
-            b'\x19Ethereum Signed Message:\n32',
-            hash
-        )
+def verify(sig: Bytes[65], msg_str: String[100], signer: address) -> bool:
+    h: bytes32 = keccak256(msg_str)
+    eth_hash: bytes32 = keccak256(
+        concat(b"\x19Ethereum Signed Message:\n32", h)
     )
     r: uint256 = convert(slice(sig, 0, 32), uint256)
     s: uint256 = convert(slice(sig, 32, 32), uint256)
     v: uint256 = convert(slice(sig, 64, 1), uint256)
-    signer: address = ecrecover(eth_signed_hash, v, r, s)
+    recovered_addr: address = ecrecover(eth_hash, v, r, s)
+    return recovered_addr == signer
 
-    return _signer == signer
